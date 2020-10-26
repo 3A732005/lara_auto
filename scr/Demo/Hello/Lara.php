8043cebc34f77f3050b24647d7e4c98fb3e85be1
li ya hsuan <ab53014698@yahoo.com.tw>
2020-10-26 22:44:08 +0800

    增加PHP檔HelloWorld,Lara,Someone的內容

---
+++ scr/Demo/Hello/Lara.php
@@ -0,0 +1,12 @@
+<?php
+namespace Demo\Hello;
+
+class Lara
+{
+    function _construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
+
+?>
---
+++ scr/Demo/HelloWorld.php
@@ -0,0 +1,12 @@
+<?php
+namespace Demo;
+
+class HelloWorld
+{
+    function _construct()
+    {
+        echo 'Hello World~'.PHP_EOL;
+    }
+}
+
+?>
