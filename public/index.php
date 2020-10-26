7c43489e400a9874ceca55a35e70f5e835d13a88
li ya hsuan <ab53014698@yahoo.com.tw>
2020-10-26 22:49:21 +0800

    套件monolog+mylog

--- public/index.php
+++ public/index.php
@@ -5,11 +5,22 @@ use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 $world=new World();
 $lara=new Lara();
 $vincent=new Hello\Someone('Vincent');
 
 $mark=new \Demo\Hello\Someone('Mark');
 $jackson=new \Demo\Hello\Someone('Jackson');
+
+// create a log channel
+$log = new Logger('3A732005');
+$log->pushHandler(new StreamHandler('../mylog/log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
 
 ?>
